1bdbb449fea369462f8730ebb048fc743b1b5171
InsearchofPandas <kylerhumphrey@gmail.com>
2020-04-07 11:38:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+
+    enum Status {
+        WATCHED
+        INTERESTED
+        NOT_INTERESTED
+        UNKNOWN
+    }
+
+
+    type Actor {
+        id: ID!
+        name: String!
+    }
+
+    type Movie {
+    id: ID!
+    title: String!
+    releaseDate: String
+    rating: Int
+    status: Status
+    actor: [Actor]
+    }
+
+    type Query {
+        movies: [Movie]
+        movie(id: ID): Movie
+    }
+`
+
+const movies = [
+    {
+        id: "289340njfksd92",
+        title: "5 Deadly Venoms",
+        releaserDate: "8-12-1978",
+        rating: 5,
+        actor: [{
+            id: "hehrhjbeuie",
+            name: "Sheng Chiang"
+        }]
+    },
+    {
+        id: '90342nfds9302fdm',
+        title: "The 36th Chamber of Shaolin",
+        releaserDate: "2-2-1978",
+        rating: 5
+    }
+];
+
+const resolvers = {
+    Query: {
+        movies: () => {
+            return movies;
+        },
+        movie: (obj, {id} , context, info) => {
+            const foundMovie = movies.find((movie) => {
+               return  movie.id === id
+            })
+            return foundMovie
+        }
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({ url }) => {
+console.log(`Server started at ${url}`)
+}); 
